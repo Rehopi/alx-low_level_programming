ccc2c86636af1f6c2efc9096b55b8359aa6c7893
rehopi <emekaokechukwu16@gmail.com>
2022-06-02 15:31:32 -0700

    add 4-puts.c to project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+
+ * main - The enty point 
+
+ *
+
+ * Return: Return O if there is no error in main program
+
+ */
+
+int main(void)
+  
+{
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+  
+}
