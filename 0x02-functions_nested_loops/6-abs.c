ef233c43675c3333a2136ed1701524d57683abd6
rehopi <emekaokechukwu16@gmail.com>
2022-06-08 16:08:02 -0700

    add 6-abs.c function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - A function that computes the absolute value of an integer.
+ *
+ * @n: is the number to compute
+ *
+ * Return: the absolue value of @n
+ */
+int _abs(int n) 
+{
+  int a = abs(n);
+  
+  return (a); 
+}
