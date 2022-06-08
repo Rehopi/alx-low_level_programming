5d86bc7377c3cc23f7814fccf418e473fa9dce51
rehopi <emekaokechukwu16@gmail.com>
2022-06-08 10:24:35 -0700

    add 1-alphabet.c to project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main- check code 
+ *
+ * Description: a function that print the alphabet in lowercase
+ * followed by a new line 
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+  int n;
+
+  for (n = 'a'; n <= 'z'; n++)
+    
+    {
+      
+      _putchar(n);
+      
+    }
+  
+  _putchar('\n'); 
+
+}
+
+    
+     
