83c0f41fd118bcd27b328f5988318ff41dc29ff5
rehopi <emekaokechukwu16@gmail.com>
2022-06-09 15:32:15 -0700

    add 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - A function that checks for a digit.
+ * @c: input
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+int a = isdigit(c);
+
+if (a == 0)
+return (a);
+else
+return (1);
+}
