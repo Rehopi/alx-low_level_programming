101a1a5ffae8d22f3bf3b7bde1e8b15d6fda28ba
rehopi <emekaokechukwu16@gmail.com>
2022-06-09 17:25:24 -0700

    fix 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,24 +5,24 @@
  * @n: input number of times '\' should be printed
  * Return: a diagonal
  */
-void print_diagonal(int n)  
+void print_diagonal(int n)
 {
-int co, sp;  
+int co, sp;
 
-if (n <= 0)    
-{      
-_putchar('\n');      
-}  
-else    
-{      
-for (co = 1; co <= n; co++)	
-{	  
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{   
+for (co = 1; co <= n; co++)
+{
 for (sp = 1; sp < co; sp++)
 {
-_putchar(' ');	      
-}	  
-_putchar('\\');	  
-_putchar('\n');	  
-}      
-} 
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
 }
