20557a6cc238cbb136967351713326d132112554
rehopi <emekaokechukwu16@gmail.com>
2022-06-09 18:18:08 -0700

    fixed

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,5 +8,18 @@
  */
 int main(void)
 {
+unsigned long num = 612852475143;
+unsigned long factor = 2;
+
+while (num)
+{
+if (num / factor > 0 && num % factor == 0)
+num /= factor;
+else if (num / factor > 0 && num % factor != 0)
+factor++;
+else
+break;
+}
+printf("%ld\n", factor);
 return (0);
 }
