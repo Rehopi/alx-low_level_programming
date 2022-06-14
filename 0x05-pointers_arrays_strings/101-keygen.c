8b8e9cdae190d01229bd4cbd826b80e21b64ea61
rehopi <emekaokechukwu16@gmail.com>
2022-06-14 14:34:51 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,22 +7,6 @@
  */
 int main(void)
 {
-int sum = 2772;
 
-char c;
-
-srand(time(NULL));
-while (sum > G_MAX)
-{
-c = rand() % (G_MAX - G_MIN) + G_MIN;
-sum -= c;
-if (sum < G_MIN)
-{
-c -= (G_MIN -sum);
-sum =G_MIN;
-}
-putchar(c);
-}
-putchar(sum);
 return(0);
 }
