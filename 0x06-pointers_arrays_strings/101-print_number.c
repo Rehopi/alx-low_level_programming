538f01e245a5918208d8898f009e96d796431a1c
rehopi <emekaokechukwu16@gmail.com>
2022-06-15 16:46:20 -0700

    101-print_number.c fixed

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,10 +6,17 @@
  */
 void print_number(int n)
 {
-int n;
 
-if(i/10!=0)
-{
-putchar(i%10);
-printnumber((i-i%10)/10);
+if (n < 0) {
+putchar('-');
+n = -n;
+}
+
+if (n == 0)
+putchar('0');
+
+if (n/10)
+printnumber(n/10);
+
+putchar(n%10 + '0');
 }
