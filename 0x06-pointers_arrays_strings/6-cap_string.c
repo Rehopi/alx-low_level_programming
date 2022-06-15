26683dc67c9213b63f39621ea6f824777be4fb70
rehopi <emekaokechukwu16@gmail.com>
2022-06-15 12:32:15 -0700

    6-cap_string.c add

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,16 +7,16 @@
  */
 char *cap_string(char *);
 {
-int i = 0, b = 0, tmp = 0;
+int i = 0, j = 0, tmp = 0;
 char sep[] = {'\n', ' ', '\t', ',', ';', '"',
 '(', ')', '{', '}', '?', '!', '.'};
 
 while (str[i] != '\0')
 {
 tmp = 0;
-for (b = 0; sep[b] != '\0' && i != 0; b++)
+for (j = 0; sep[j] != '\0' && i != 0; j++)
 {
-if (str[i - 1] == sep[b])
+if (str[i - 1] == sep[j])
 {
 tmp = 1;
 break;
