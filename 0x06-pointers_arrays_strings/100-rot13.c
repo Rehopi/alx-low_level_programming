728d6f52d1f06ce3da89a9656c248b281dedf33c
rehopi <emekaokechukwu16@gmail.com>
2022-06-15 15:51:39 -0700

    100-rot13.cfixed

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,7 +12,7 @@ int i, j;
 
 char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-  
+
 for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; a[j] != '\0'; j++)
