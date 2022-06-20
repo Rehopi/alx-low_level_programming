94fdecc1e9fabfa6b044d096a2427a38293fa6e1
rehopi <emekaokechukwu16@gmail.com>
2022-06-20 05:28:11 -0700

    add 0-memset.c program

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,13 @@
 #include main.h
 /**
- * _memset - fills memory with a constant byte
+ * _memset - fills memory with a constant byte.
  * @s: the address of the memory to fill
  * @b: the constant byte to fill
  * @n: number of bytes to fill
  *
  * Return: a pointer to the address of the filled memory
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 
