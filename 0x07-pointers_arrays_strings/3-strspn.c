58a9994e6198bc173ffb5b3e87520fd2e1ce0c6d
rehopi <emekaokechukwu16@gmail.com>
2022-06-20 06:20:03 -0700

    add 3-strspn.c file

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept) 
+{
+unsigned int i, n, point, result;
+
+point = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+result = 0;
+
+for (n = 0; accept[n] != '\0'; n++)
+{
+if (accept[n] == s[i])
+{
+point++;
+result = 1;
+}
+}
+     
+if (result == 0)
+return (point);
+}
+
+return (point);
+}
