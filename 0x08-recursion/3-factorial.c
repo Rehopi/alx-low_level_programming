3169293d02d8b0381b5b4c6708d327888c900363
rehopi <emekaokechukwu16@gmail.com>
2022-06-22 04:48:34 -0700

    add 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,15 +7,14 @@
  *
  * Return: the factorial of @n
  */
+
 int factorial(int n);
 {
 if (n < 0)
 return(-1);
 
-else if ( n == 0)
+else if (n == 0)
 return(1);
 
-return (n * factorial(n - 1))
+return (n * factorial(n - 1));
 }
-   
-    
