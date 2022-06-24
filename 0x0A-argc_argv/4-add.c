a0381f2257417962fe0e2b4cda87cfa55e9fe284
rehopi <emekaokechukwu16@gmail.com>
2022-06-24 06:26:43 -0700

    add 4-add.c program

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,13 +13,12 @@ int i, sum = 0;
 
 if (argc < 1)
 return (0);
-
 for (i = 1; i < argc; i++)
 {
 if (!atoi(argv[i]))
 {
 printf("%s\n", "Error");
-return(1);
+return (1);
 }
 sum += atoi(argv[i]);
 }
