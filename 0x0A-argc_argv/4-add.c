9340bea670c04b261104771629ec07772609a395
rehopi <emekaokechukwu16@gmail.com>
2022-06-24 08:43:03 -0700

    add 4-add.c to program

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,39 +1,29 @@
 #include <stdio.h>
 #include <stdlid.h>
-#include "main.h"
-
+#iclude "main.h"
 /**
- * main - adds positive numbers
- *
- * @argc: argument counter
- * @argv: argument vector
- *
- * Return: 1 if ERROR such as symbols that are not numbers,
- * 0 if nothing is passed
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) 
 {
-int c, d;
-int n = 0;
-
-if (argc < 2)
-{
-printf("0\n");
+int i, sum = 0;
+  
+if (argc < 1)
 return (0);
-}
 
-for (c = 1; c < argc; c++)
+for (i = 1; i < argc; i++)
 {
-for (d = 0; argv[c][d] != '\0'; d++)
+if (!atoi(argv[i]))
 {
-if (argv[c][d] < '0' || argv[c][d] > '9')
-{
-printf("Error\n");
+printf("%s\n", "Error");
 return (1);
 }
+sum += atoi(argv[i]);
 }
-n += atoi(argv[c]);
-}
-printf("%d\n", n);
+printf("%d\n", sum);
+
 return (0);
 }
