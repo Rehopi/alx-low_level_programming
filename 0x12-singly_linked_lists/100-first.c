f3cab73d79574114ec263546af09676590d24592
rehopi <emekaokechukwu16@gmail.com>
2022-07-14 20:12:20 -0700

    add 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
-#include "lists.h"
+#include <stdio.h>
+
 /**
- * startupfun - runs before main function
- *
- * Return: Nothing 
+ * execute_before_main - executes this function before main function
+ * Note: made possible by prior declaration of "__attribute__ ((constructor))"
  */
-void startupfun(void)
+
+void __attribute__ ((constructor)) execute_before_main(
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
