e70c04041631aa7acf4101e3e0142e602976a989
rehopi <emekaokechukwu16@gmail.com>
2022-07-14 17:25:24 -0700

    fixed betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,13 +7,13 @@
  */
 int _slen(char *str)
 {
-	int len = 0;
+int len = 0;
 
-	if (str == NULL)
-		return (0);
-	while (*(str + len) != '\0')
-		len++;
-	return (len);
+if (str == NULL)
+return (0);
+while (*(str + len) != '\0')
+len++;
+return (len);
 }
 
 /**
@@ -24,20 +24,20 @@ int _slen(char *str)
  */
 size_t print_list(const list_t *h)
 {
-	const list_t *cnode;
-	size_t i = 0;
-	int len;
+const list_t *cnode;
+size_t i = 0;
+int len;
 
-	if (h == NULL)
-		return (0);
-	cnode = h;
-	while (cnode != NULL)
-	{
-		len = (cnode->str) ? _slen(cnode->str) : 0;
-		printf("[%d] %s\n", len, (cnode->str) ? cnode->str : "(nil)");
-		cnode = cnode->next;
-		i++;
-	}
+if (h == NULL)
+return (0);
+cnode = h;
+while (cnode != NULL)
+{
+len = (cnode->str) ? _slen(cnode->str) : 0;
+printf("[%d] %s\n", len, (cnode->str) ? cnode->str : "(nil)");
+cnode = cnode->next;
+i++;
+}
 
-	return (i);
+return (i);
 }
