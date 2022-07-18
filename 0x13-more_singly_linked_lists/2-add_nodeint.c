c118ae1052a50433c92489d9dc6b4bb98f331b70
rehopi <emekaokechukwu16@gmail.com>
2022-07-18 16:25:31 -0700

    2-add_nodeint.c fixed

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: the head node of the list 
+ * @head: the head node of the list
  * @n: the data to add on the node
  * Return: the address of the new element, or NULL if it failed
  */
