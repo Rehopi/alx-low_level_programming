1808955a7f2da8464b150f73e8b09faff9933724
rehopi <emekaokechukwu16@gmail.com>
2022-07-18 15:52:47 -0700

    add 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,6 @@
 /**
  * print_listint - prints all the elements of a listint_t list
  * @h: head of the list
- *
  * Return: the number of nobes
  */
 
@@ -11,7 +10,7 @@ size_t print_listint(const listint_t *h)
 {
 	size_t counter = 0;
 
-	while(h)
+	while (h)
 	{
 		printf("%i\n", h->n);
 		h = h->next;
