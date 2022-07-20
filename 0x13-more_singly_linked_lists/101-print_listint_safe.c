735ac78a9a9b1a4c40c742507f5bf8968be9bdb5
rehopi <emekaokechukwu16@gmail.com>
2022-07-20 08:56:42 -0700

    101-print_listint_safe.c fixed

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,37 +1,36 @@
 #include "lists.h"
 /**
- * print_listint_safe - prints a listint_t linked list
+ * print_listint_safe - prints all elements of listint
  * @h: pointer to first node of list
- * Return:  num of elements
+ * Return: num of elements
  */
-size_t print_listint_safe(const listint_t *head)
+size_t print_listint_safe(const listint_t *h)
 {
-	int i;
-	int j;
-	const listint_t *tmp;
-	const listint_t *current;
+int i;
+int j;
+const listint_t *tmp;
+const listint_t *current;
 
-	temp = h;
+tmp = h;
 
-	i = 0;
-	while (tmp != NULL)
-	{
-		current = h;
-		j = 0;
-		while (j < i)
-		{
-			if (current == tmp)
-			{
-				printf("-> [%p] %d\n", (void *) tmp, tmp->n);
-				return (i);
-			}
-			current = current->next;
-			j++;
-		}
-		printf("[%p] %d\n", (void *) tmp, tmp->n);
-		tmp = tmp->next;
-		i++;
-	}
-	return (i);
+i = 0;
+while (tmp != NULL)
+{
+current = h;
+j = 0;
+while (j < i)
+{
+if (current == tmp)
+{
+printf("-> [%p] %d\n", (void *) tmp, tmp->n);
+return (i);
 }
-	
+current = current->next;
+j++;
+}
+printf("[%p] %d\n", (void *) tmp, tmp->n);
+tmp = tmp->next;
+i++;
+}
+return (i);
+}
