517ef7ed954600785f8c65c2187dc602463f4cad
rehopi <emekaokechukwu16@gmail.com>
2022-07-21 16:21:50 -0700

    main.h fixed

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,10 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
+/*Functions*/
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
