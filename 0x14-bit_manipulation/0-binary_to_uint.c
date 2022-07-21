d6624d49eec0379efdf50a099f9b5ee92c8997d2
rehopi <emekaokechukwu16@gmail.com>
2022-07-21 16:35:32 -0700

    0-binary_to_uint.c file fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,49 @@
 #include "main.h"
+
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: string of the binary number
- * Return: the converted number, or 0 if
+ * _pow - pows the two numbers
+ * @base: unsigned int value input
+ * @exp: unsigned int value input
+ * Return: row the two numbers
+ */
+unsigned int _pow(unsigned int base, unsigned int exp)
+{
+	unsigned int i;
+
+	if ( exp == 0)
+		return (1);
+	for (i = 1; i < exp; i++)
+		base += base;
+	return (base);
+}
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: is pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0 if:
+ * there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n;
-	int i, j;
+	unsigned int exp = 0, base = 2, weight = 0, base10 = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	i = 0;
-	while (b[i + 1] != '\0')
-		i++;
-	j = 1;
-	n = 0;
-	while (i >= 0)
+
+	for (; *(b + exp) != '\0'; exp++)
+		;
+	--exp;
+	while (*b)
 	{
-		if (b[i] != '1' && b[i] != '0')
+		if (*b == '0' || *b == '1')
+		{
+			weight = (*b - '0') * _pow(base, exp);
+			base10 += weight;
+			--exp;
+		}
+		else
 			return (0);
-		n += (b[i] - '0') * j;
-		j *= 2;
-		i--;
+		b++;
 	}
-	return (i);
+	return (base 10);
 }
