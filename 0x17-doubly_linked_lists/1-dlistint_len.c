a0c6d40b6fb4488233112a26466cfaf3cb211ad1
rehopi <emekaokechukwu16@gmail.com>
2022-09-01 15:28:15 -0700

    function that returns the number of elements in a linked dlistint_t list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,7 +5,7 @@
  * Return: number of elements in a linked list
  */
 
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 	const dlistint_t *temp = h;
